b9bbc2be35ea3bd1d971126c3706f1c3195224ef
sebvc <90735870+sebvc@users.noreply.github.com>
2023-03-27 20:23:48 -0500

    Latest Improvemtns - updated bootstrap to 5.3.0-alpha2 - added quotes - major working changes on needed js for stats and quotes handling

--- js/stats.js
+++ js/stats.js
@@ -1,78 +1,79 @@
-var stats = document.getElementById("stats");
 function MyFunction() {
-    var URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsfAXl5a5hyjA4Fuo0kt4s7vjvFr6PwbeAUJsyhObEKvciz9l1Gu-urb_9rDzAzQqaqzWAr1GDTk6O/pub?gid=1228600978&single=true&output=csv";
+  var URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsfAXl5a5hyjA4Fuo0kt4s7vjvFr6PwbeAUJsyhObEKvciz9l1Gu-urb_9rDzAzQqaqzWAr1GDTk6O/pub?gid=1228600978&single=true&output=csv";
 
-    return new Promise(function (resolve, reject) {
-        $.ajax({ url: URL, async: false })
-            .done(function (result) {
-                // document.getElementById("demo").innerHTML = result + '<br>';
-                resolve(result);
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                reject(errorThrown);
-            });
-    });
+  return new Promise(function (resolve, reject) {
+    $.ajax({ url: URL, async: false })
+      .done(function (result) {
+        // document.getElementById("demo").innerHTML = result + '<br>';
+        resolve(result);
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        reject(errorThrown);
+      });
+  });
 }
+
 // function MyFunction() {
 //     var URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsfAXl5a5hyjA4Fuo0kt4s7vjvFr6PwbeAUJsyhObEKvciz9l1Gu-urb_9rDzAzQqaqzWAr1GDTk6O/pub?gid=1228600978&single=true&output=csv";
 
-//     var stringvar = "test";
+function run_stats() {
 
-//     $.ajax({ url: URL, async: false }).done(function (result) { stringvar = result; });
-//     console.log(stringvar)
-//     /*
-//     Name,Amount,IconHTML
-//     PLACES LIVED,6,"<i class=""fa-solid fa-map-location-dot""></i>"
-//     LIVES CHANGED,684,"<i class=""fa-solid fa-users""></i>"
-//     DOORS OPENED,79205,"<i class=""fa-solid fa-door-open""></i>"
-//     CONTRIES VISITED,13,"<i class=""fa-solid fa-earth-americas""></i>"
-//     BURRITOS EATEN,1356,"<img type=""image/png"" src=""images/burrito.png""></img>"
-//     CREDIT HOURS,182,"<i class=""icon-clock""></i>"
-//     */
-//     return stringvar;
-
-// }
-MyFunction().then(result => {
+/*     var stats = document.getElementById("stats");
+     */    MyFunction().then(result => {
     return csv().fromString(result);
-}).then(function (csv) {
-    // console.log("HELLOOOOOOOOO")
-    // console.log(csv)
-    // stats.innerHTML = "<code>" + JSON.stringify(csv) + "</code>";
-    
-    /*
-    stats.innerHTML += "<div id='stat_table' class=\"stats-list d-flex gap-1 flex-wrap align-content-center justify-content-center pt-2 pb-4\">";
-    var stat_table = document.getElementById("stat_table");
-    csv.forEach(function (row) {
-        stat_table.innerHTML += "<div class=\"border w-25 flex-fill bgrid stat p-2\">"
-            + "<div style=\"font-size: 5em\" class=\"icon-part text-black\">" + "<i class=\"" + row.IconClass + "\"></i>" + "</div>"
-            + "<p style=\"font-size: 3em\" class=\"stat-count fw-bolder\">" + row.Amount + "</p>"
-            + "<p class=\"stat-title h5 text-uppercase fw-bolder\">" + row.Name + "</p>"
-            + "</div>";
-    });
-    */
-    /*
-    stats.innerHTML += "<div id='stat_table' class=\"stats-list row align-content-center justify-content-center pt-2 pb-4\">";
-    var stat_table = document.getElementById("stat_table");
-    csv.forEach(function (row) {
-        stat_table.innerHTML += "<div class=\"col-sm-6 col-md-4 col-lg-2 border stat\">"
-            + "<div style=\"font-size: 4.5em\" class=\"icon-part text-black\">" + "<i class=\"" + row.IconClass + "\"></i>" + "</div>"
-            + "<p style=\"font-size: 2.5em\" class=\"h1 stat-count fw-bolder\">" + row.Amount + "</p>"
-            + "<p class=\"stat-title h5 text-uppercase fw-bolder\">" + row.Name + "</p>"
-            + "</div>";
-    });
-    */
-    stats.innerHTML += "<div id='stat_table' class=\"stats-list row align-content-center justify-content-center py-4 mb-3\">";
-    var stat_table = document.getElementById("stat_table");
-    csv.forEach(function (row) {
-        stat_table.innerHTML += "<div class=\"stat col-12 col-sm-6 col-md-4 col-lg-2\">"
-            + "<div class=\"icon-part mt-4 pt-2 mb-2\">" + "<i class=\"" + row.IconClass + "\"></i>" + "</div>"
-            + "<p class=\"stat-count h1 fw-bolder\">" + row.Amount + "</p>"
-            + "<p class=\"stat-title h5 fw-bolder\">" + row.Name + "</p>"
-            + "</div>";
-    });
+    /* return csvToJSON(result); */
+  }).then(function (csv) {
+    var icons = document.querySelectorAll(".icon-part");
+    var values = document.querySelectorAll(".stat-count");
+    var titles = document.querySelectorAll(".stat-title");
+    const num = icons.length;
+    const num2 = values.length;
+    const num3 = titles.length;
+    console.log(num)
+
+    for (let i = 0; i < num; i++) {
+      /* console.log(values[i].innerHTML); */
+      console.log(icons[i].innerHTML);
+      console.log(csv[i].IconHTML);
+      console.log(values[i].innerHTML);
+      console.log(titles[i].innerHTML);
+      icons[i].innerHTML = csv[i].IconHTML;
+      values[i].innerHTML = csv[i].Amount;
+      titles[i].innerHTML = csv[i].Name;
+    }
+    // return Promise.resolve("stat_table Updated");
+    /* csv.forEach(function (row) {
+        console.log(row.Name);
+        // stat_table.innerHTML += "<div class=\"stat col-12 col-sm-6 col-md-4 col-lg-2\">"
+        //     + "<div class=\"icon-part mt-4 pt-2 mb-2\">" + "<i class=\"" + row.IconClass + "\"></i>" + "</div>"
+        //     + "<p class=\"stat-count h1 fw-bolder\">" + row.Amount + "</p>"
+        //     + "<p class=\"stat-title h5 fw-bolder\">" + row.Name + "</p>"
+        //     + "</div>";
+    }); */
     // Do something with the result
-}).catch(function (error) {
+  }).catch(function (error) {
     // Handle the error
     console.log("HEY SEB you have caught an error:")
     console.log(error)
-});
+  });
+
+}
+
+// <div id="quotes" class="d-block container py-2">
+    // <div id="stats" class="d-none count-up container py-2">
+    // SWAP d-block and d-none in stats and quotes elements
+    function toggleStats() {
+      var stats_block = document.querySelector("#stats");
+      // if (stats_block == null) {
+      //   setTimout( () => {
+      //     stats_block = document.querySelector("#stats");
+      //     console.log("inside timeout");
+      //     console.log(stats_block)
+
+      //   }, 1000) 
+      // }
+      console.log(stats_block)
+      stats_block.classList.toggle("d-none");
+      stats_block.classList.toggle("d-block");
+    }
+      
