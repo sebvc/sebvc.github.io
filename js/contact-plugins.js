069de85991e16d267332a497ba9c494e8a8588af
Villa Cuellar <sebvc@auth.tamu.edu>
2023-05-08 01:40:08 -0500

    Fixed issues with people.tamu.edu rendering

--- js/contact-plugins.js
+++ js/contact-plugins.js
@@ -25,8 +25,98 @@
  * @copyright James Padolsey http://james.padolsey.com
  * @version 1.04
  */
-var e;(e=jQuery).fn.autoResize=function(t){var i=e.extend({onResize:function(){},animate:!0,animateDuration:150,animateCallback:function(){},extraSpace:60,limit:1e3},t);return this.filter("textarea").each((function(){var t,n=e(this).css({resize:"none"}),a=n.height(),o=(t={},e.each(["height","width","lineHeight","textDecoration","letterSpacing"],(function(e,i){t[i]=n.css(i)})),n.clone().removeAttr("id").removeAttr("name").css({position:"absolute",top:0,left:-9999}).css(t).attr("tabIndex","-1").insertBefore(n)),s=null,c=function(){o.height(0).val(e(this).val()).scrollTop(1e4);var t=Math.max(o.scrollTop(),a)+i.extraSpace,c=e(this).add(o);s!==t&&(s=t,t>=i.limit?e(this).css("overflow-y",""):(i.onResize.call(this),i.animate&&"block"===n.css("display")?c.stop().animate({height:t},i.animateDuration,i.animateCallback):c.height(t)))};n.unbind(".dynSiz").bind("keyup.dynSiz",c).bind("keydown.dynSiz",c).bind("change.dynSiz",c)})),this},
-/*
- * jQuery autoResize (textarea auto-resizer) (ORIGINAL)
-function(e){e.fn.autoResize=function(t){var i=e.extend({onResize:function(){},animate:!0,animateDuration:150,animateCallback:function(){},extraSpace:20,limit:1e3},t);return this.filter("textarea").each((function(){var t,n=e(this).css({resize:"none","overflow-y":"hidden"}),a=n.height(),o=(t={},e.each(["height","width","lineHeight","textDecoration","letterSpacing"],(function(e,i){t[i]=n.css(i)})),n.clone().removeAttr("id").removeAttr("name").css({position:"absolute",top:0,left:-9999}).css(t).attr("tabIndex","-1").insertBefore(n)),s=null,c=function(){o.height(0).val(e(this).val()).scrollTop(1e4);var t=Math.max(o.scrollTop(),a)+i.extraSpace,c=e(this).add(o);s!==t&&(s=t,t>=i.limit?e(this).css("overflow-y",""):(i.onResize.call(this),i.animate&&"block"===n.css("display")?c.stop().animate({height:t},i.animateDuration,i.animateCallback):c.height(t)))};n.unbind(".dynSiz").bind("keyup.dynSiz",c).bind("keydown.dynSiz",c).bind("change.dynSiz",c)})),this}}(jQuery);
-*/
+
+(function ($) {
+
+    $.fn.autoResize = function (options) {
+
+        // Just some abstracted details,
+        // to make plugin users happy:
+        var settings = $.extend({
+            onResize: function () { },
+            animate: true,
+            animateDuration: 150,
+            animateCallback: function () { },
+            extraSpace: 60,
+            limit: 1000
+        }, options);
+
+        // Only textarea's auto-resize:
+        this.filter('textarea').each(function () {
+
+            // Get rid of scrollbars and disable WebKit resizing:
+            var textarea = $(this).css({ resize: 'none' }),
+            //var textarea = $(this).css({ resize: 'none', 'overflow-y': 'hidden' }),
+
+
+                // Cache original height, for use later:
+                origHeight = textarea.height(),
+
+                // Need clone of textarea, hidden off screen:
+                clone = (function () {
+
+                    // Properties which may effect space taken up by chracters:
+                    var props = ['height', 'width', 'lineHeight', 'textDecoration', 'letterSpacing'],
+                        propOb = {};
+
+                    // Create object of styles to apply:
+                    $.each(props, function (i, prop) {
+                        propOb[prop] = textarea.css(prop);
+                    });
+
+                    // Clone the actual textarea removing unique properties
+                    // and insert before original textarea:
+                    return textarea.clone().removeAttr('id').removeAttr('name').css({
+                        position: 'absolute',
+                        top: 20,
+                        //top: 0,
+                        left: -9999
+                    }).css(propOb).attr('tabIndex', '-1').insertBefore(textarea);
+
+                })(),
+                lastScrollTop = null,
+                updateSize = function () {
+
+                    // Prepare the clone:
+                    clone.height(0).val($(this).val()).scrollTop(10000);
+
+                    // Find the height of text:
+                    var scrollTop = Math.max(clone.scrollTop(), origHeight) + settings.extraSpace,
+                        toChange = $(this).add(clone);
+
+                    // Don't do anything if scrollTip hasen't changed:
+                    if (lastScrollTop === scrollTop) { return; }
+                    lastScrollTop = scrollTop;
+
+                    // Check for limit:
+                    if (scrollTop >= settings.limit) {
+                        $(this).css('overflow-y', '');
+                        return;
+                    }
+                    // Fire off callback:
+                    settings.onResize.call(this);
+
+                    // Either animate or directly apply height:
+                    settings.animate && textarea.css('display') === 'block' ?
+                        toChange.stop().animate({ height: scrollTop }, settings.animateDuration, settings.animateCallback)
+                        : toChange.height(scrollTop);
+                };
+
+            // Bind namespaced handlers to appropriate events:
+            textarea
+                .unbind('.dynSiz')
+                .bind('keyup.dynSiz', updateSize)
+                .bind('keydown.dynSiz', updateSize)
+                .bind('change.dynSiz', updateSize);
+
+        });
+
+        // Chain:
+        return this;
+
+    };
+
+
+
+})(jQuery);
+
