6ba4576f1b8aa659f23d4b74179b46c72b202d01
sebvc <90735870+sebvc@users.noreply.github.com>
2023-06-24 10:51:19 -0500

    standarized tab size: 4 in most files

--- js/contact.js
+++ js/contact.js
@@ -3,24 +3,24 @@
  * main js for contact.html
  *
  * ------------------------------------------------------------------- 
- */ 
+ */
 
-(function($) {
+(function ($) {
 
 	"use strict";
 
 
-   /*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
-	$('input, textarea, select').placeholder()  
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()
 
-   /*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
 	$('textarea').autoResize();
 
-  	/*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/*	contact form
 	------------------------------------------------------ */
 
@@ -28,47 +28,47 @@
 	$('#contactForm').validate({
 
 		/* submit via ajax */
-		submitHandler: function(form) {
+		submitHandler: function (form) {
 
 			var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+			$.ajax({
 
-		      type: "POST",
-		      url: "php/email_sender.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
+				type: "POST",
+				url: "php/email_sender.php",
+				data: $(form).serialize(),
+				beforeSend: function () {
 
-		      	sLoader.fadeIn(); 
+					sLoader.fadeIn();
 
-		      },
-		      success: function(msg) {
+				},
+				success: function (msg) {
 
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
+					// Message was sent
+					if (msg == 'OK') {
+						sLoader.fadeOut();
+						$('#message-warning').hide();
+						$('#contactForm').fadeOut();
+						$('#message-success').fadeIn();
+					}
+					// There was an error
+					else {
+						sLoader.fadeOut();
+						$('#message-warning').html(msg);
+						$('#message-warning').fadeIn();
+					}
 
-		      },
-		      error: function() {
+				},
+				error: function () {
 
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
+					sLoader.fadeOut();
+					$('#message-warning').html("Something went wrong. Please try again.");
+					$('#message-warning').fadeIn();
 
-		      }
+				}
 
-	      });     		
-  		}
+			});
+		}
 
 	});
 
--- js/quotes.js
+++ js/quotes.js
@@ -1,52 +1,52 @@
 // Quote Selector
 function selectQuote() {
 
-    // let rand_idx = Math.floor((Math.random() * 5) + 1); /* Use if have heading */
-    let rand_idx = Math.floor((Math.random() * 5));
-    // console.log(rand_idx);
-    //   let quote_block = document.querySelector("#quotes");
-    let quote_block = document.getElementById("quotes");
-    if (quote_block == null) {
-        quote_block = document.querySelector("section#quotes_sec");
-        // console.log("Hello1");
-        if (quote_block == null) {
-            quote_block = document.querySelector("div#quotes");
-            // console.log("Hello2");
-        }
-        let kids = quote_block.childNodes;
+	// let rand_idx = Math.floor((Math.random() * 5) + 1); /* Use if have heading */
+	let rand_idx = Math.floor((Math.random() * 5));
+	// console.log(rand_idx);
+	//   let quote_block = document.querySelector("#quotes");
+	let quote_block = document.getElementById("quotes");
+	if (quote_block == null) {
+		quote_block = document.querySelector("section#quotes_sec");
+		// console.log("Hello1");
+		if (quote_block == null) {
+			quote_block = document.querySelector("div#quotes");
+			// console.log("Hello2");
+		}
+		let kids = quote_block.childNodes;
 
-        console.log(kids[2]);
+		console.log(kids[2]);
 
 
-        for (let index = 0; index < quote_block.childNodes.length; index++) {
-            const element = array[index];
-            // console.log(kids[index]);
-        }
-        // console.log("Bye");
-        // console.log(quote_block.children);
-        let quote_children = quote_block.childNodes.values();
-        // console.log(quote_children);
-        // quote_block = quote_block.classList
-    } else {
+		for (let index = 0; index < quote_block.childNodes.length; index++) {
+			const element = array[index];
+			// console.log(kids[index]);
+		}
+		// console.log("Bye");
+		// console.log(quote_block.children);
+		let quote_children = quote_block.childNodes.values();
+		// console.log(quote_children);
+		// quote_block = quote_block.classList
+	} else {
 
-    }
-    // console.log("Bye2");
-    // console.log(quote_block.children);
-    // for (let i = 1; i < quote_block.children.length; i++) { /* use if have heading */
-    for (let i = 0; i < quote_block.children.length; i++) {
-        if (i == rand_idx) {
-            // console.log(rand_idx);
-            // Quote_Cards[i].className = Quote_Cards[i].className.add('d-none');
-        } else {
-            quote_block.children[i].classList.add("d-none");
-        }
-    }
-    // console.log("Quote #^ shone");
+	}
+	// console.log("Bye2");
+	// console.log(quote_block.children);
+	// for (let i = 1; i < quote_block.children.length; i++) { /* use if have heading */
+	for (let i = 0; i < quote_block.children.length; i++) {
+		if (i == rand_idx) {
+			// console.log(rand_idx);
+			// Quote_Cards[i].className = Quote_Cards[i].className.add('d-none');
+		} else {
+			quote_block.children[i].classList.add("d-none");
+		}
+	}
+	// console.log("Quote #^ shone");
 
 }
 
 function toggleQuotes() {
-    var quote_block = document.getElementById("quotes");
-    quote_block.classList.toggle("d-none");
-    quote_block.classList.toggle("d-block");
+	var quote_block = document.getElementById("quotes");
+	quote_block.classList.toggle("d-none");
+	quote_block.classList.toggle("d-block");
 }
