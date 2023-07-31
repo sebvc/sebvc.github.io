99e599d2746ef65c114797f8239582dfc4a72861
Sebastian Villa <90735870+sebvc@users.noreply.github.com>
2023-07-31 00:32:27 -0500

    Email/Contact Google Form implementation  - https://docs.google.com/forms /d/e/1FAIpQLSdjPnpfabaYdgm5YULcs4navaA4Y4eRkccHktEZHE8X30YuNg/viewform  - fix white space errors  + Todo: change modal launcher to manual show after form validation  + Todo: why incorrect form won't partially populate gooleform in iframe  + Todo: fix textarea JS resize for include-snippet of contact section

--- style/js/contact.js
+++ style/js/contact.js
@@ -5,6 +5,46 @@
  * ------------------------------------------------------------------- 
  */
 
+// $('#gform').on('submit', function (e) {
+//     $('#gform *').fadeOut(2000);
+//     $('#gform').prepend('Your submission has been processed...');
+// });
+
+////////////////////////////////////////////////
+// const contactForm = document.getElementById('contactForm'); // <form>
+// const googleModal = document.getElementById('googleModal'); // modal
+// const modalLauncher = document.getElementById('modalLauncher'); // modal launch button
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	// const contactForm = document.getElementById('contactForm'); // <form>
+	const hidden_iframe = document.getElementById('hidden_iframe'); // google form iframe inside modal
+	// const googleModal = document.getElementById('googleModal'); // modal
+	// const modalLauncher = document.getElementById('modalLauncher'); // modal launch button
+
+	const emailValue = document.getElementById('entry.354116389').value; // E-Mail
+	const nameValue = document.getElementById('entry.1109242279').value; // Name
+	const subjectValue = document.getElementById('entry.751595604').value; // Subject
+	const messageValue = document.getElementById('entry.2024228114').value; // message body
+
+	// Construct the Google Form URL with the custom query parameters
+	const baseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSdjPnpfabaYdgm5YULcs4navaA4Y4eRkccHktEZHE8X30YuNg/formResponse'; // Replace with your target URL
+	const queryParams = `?usp=pp_url&entry.354116389=${encodeURIComponent(emailValue)}&entry.1109242279=${encodeURIComponent(nameValue)}&entry.751595604=${encodeURIComponent(subjectValue)}&entry.2024228114=${encodeURIComponent(messageValue)}`;
+	const targetURL = baseURL + queryParams;
+
+	// Navigate to the generated URL
+	// window.location.href = targetURL; // opens a new tab
+	hidden_iframe.src = targetURL; // renders in <iframe id="hidden_iframe">
+	// googleModal.show(modalLauncher);
+	$('#modalLauncher').fadeIn(750);
+	$('#contactForm').fadeOut(750);
+	// contactForm.style = "display:none;";
+
+});
+
+////////////////////////////////////////////////
+
+
 (function ($) {
 
 	"use strict";
@@ -19,7 +59,7 @@
 	/*  Placeholder Plugin Settings
 	------------------------------------------------------ */
 	$('textarea').autoResize();
-	
+
 	/*---------------------------------------------------- */
 	/*	modal form
 	------------------------------------------------------ */
@@ -27,7 +67,7 @@
 	/*---------------------------------------------------- */
 	/*	contact form
 	------------------------------------------------------ */
-	
+
 	/*---------------------------------------------------- */
 	/*	local validation 
 	/*---------------------------------------------------- */
